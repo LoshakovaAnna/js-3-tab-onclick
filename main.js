95d707ff8de4149e689043b1e605b7c3157027d9
LoshakovaAnna <ann.loshakova.111@gmail.com>
2018-10-25 12:36:46 +0300

    add classList(add/remove), add img, improve style

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-/*function ready() {
-    alert( 'DOM готов' );
-   
-  }
 document.addEventListener("DOMContentLoaded", ready);
-var elements = document.getElementsByClassName('numbers-table');
-function clickfunct() {
-   console.log('it work');
-  }
-elements.addEventListener("onclick", clickfunct);*/
 
-document.addEventListener("DOMContentLoaded", ready);
-    function ready(){
-        let windList = document.querySelectorAll(".window");
-        let tabList = document.querySelectorAll(".number-cell");
-        let tab = document.querySelector(".numbers-table");
-        tab.addEventListener('click', function(e) {
-            console.log('addEventListener onClick Work!!');
-            let i = [...tabList].indexOf(e.target);
-            console.log('tab.indexOf = '+i);
-            windList.forEach(windList => {
-                this.classList.remove(".window-active");
-                console.log('remove Work!!');
-            });
-            windList[i].classList.remove(".window");
-            windList[i].classList.add(".window-active");
-           
+function ready(){
+    let windList = document.querySelectorAll(".window");
+    let tabList = document.querySelectorAll(".number-cell");
+
+    let tab = document.querySelector(".numbers-table");
+
+    tab.addEventListener('click', function(e) {
+        console.log('addEventListener onClick Work!!');
+        let i = [...tabList].indexOf(e.target);
+        console.log('tab.indexOf = '+i);
+        for (let j = 0; j < [...windList].length; j++) {               
+            [...windList][j].classList.remove("window-active");
+            [...windList][j].classList.add("window-hide");                
+        }          
+
+        [...windList][i].classList.remove("window-hide");
+        [...windList][i].classList.add("window-active");           
 
     })
 }
