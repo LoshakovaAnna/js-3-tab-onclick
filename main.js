781a8f8659ee3c84feae8a2e5bc381e1ee8d5d15
LoshakovaAnna <42207909+LoshakovaAnna@users.noreply.github.com>
2018-10-29 22:01:31 +0300

    delete console.lof, change [..]

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready(){
-    let windList = document.querySelectorAll(".window");
-    let tabList = document.querySelectorAll(".number-cell");
+    let windList = [...document.querySelectorAll(".window")];
+    let tabList = [...document.querySelectorAll(".number-cell")];
 
     let tab = document.querySelector(".numbers-table");
 
     tab.addEventListener('click', function(e) {
-        console.log('addEventListener onClick Work!!');
-        let i = [...tabList].indexOf(e.target);
-        console.log('tab.indexOf = '+i);
+        
+        let i = tabList.indexOf(e.target);
+       
         for (let j = 0; j < [...windList].length; j++) {               
-            [...windList][j].classList.remove("window-active");
-            [...windList][j].classList.add("window-hide");                
+            windList[j].classList.remove("window-active");
+            windList[j].classList.add("window-hide");                
         }          
 
-        [...windList][i].classList.remove("window-hide");
-        [...windList][i].classList.add("window-active");           
+        windList[i].classList.remove("window-hide");
+        windList[i].classList.add("window-active");           
 
     })
 }
